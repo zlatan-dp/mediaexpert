f8e1407f745b0b96bee7515bd6602c6ec0a22809
zlatandp <zlatandnepr@gmail.com>
2024-11-11 15:34:09 +0200

    add responsive, add politika

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,10 @@
     const refs = {
       openModalBtns: document.querySelectorAll('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
+      openTermsBtn: document.querySelector('[data-terms-open]'),
+      closeTermsBtn: document.querySelector('[data-terms-close]'),
       modal: document.querySelector('[data-modal]'),
+      modalTerms: document.querySelector('[data-modal-terms]'),
       body: document.querySelector('body'),
       backdrop: document.querySelector('.backdrop'),
     };
@@ -13,6 +16,9 @@
 
     refs.closeModalBtn.addEventListener('click', toggleModal);
 
+    refs.openTermsBtn.addEventListener('click', toggleTermsModal);
+    refs.closeTermsBtn.addEventListener('click', toggleTermsModal);
+
     refs.backdrop.addEventListener('click', (event) => {
         if (event.target === refs.backdrop) { 
             toggleModal();
@@ -22,5 +28,9 @@
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
       refs.body.classList.toggle('no-scroll');
+    }
+
+    function toggleTermsModal() {
+      refs.modalTerms.classList.toggle('is-hidden');
     }
   })();
